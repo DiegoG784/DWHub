c22cc6a1369e69943ab0db70f0fbbe1ea926203a
DiegoG784 <143761719+DiegoG784@users.noreply.github.com>
2025-11-15 09:48:12 -0300

    added commentaries throughout files

--- js/styling/common_style_loader.js
+++ js/styling/common_style_loader.js
@@ -1,4 +1,7 @@
+//Uma espécie de framework no estilo Bootstrap que fiz especificamente para este portfólio devido ao design do mesmo.
+
 function loadCommonStyler() {
+    //Todos os arquivos listados a baixo se encontram na pasta que foi definida na variável seguinte
     let commonStyleFiles = [
         'flex.css',
         'margin.css',
@@ -10,10 +13,13 @@ function loadCommonStyler() {
 
 
     commonStyleFiles.forEach(file => {
-        // console.log(document.URL);
+        //Cria um elemento <link> com o arquivo acima atribuido e inserido dentro do head do site
         let linkElement = document.createElement('link');
         linkElement.rel = 'stylesheet';
 
+        //Devido ao Github não hospedar um site com o repositório atual no diretório raiz,
+        //  eu fiz uma condição que checa se a URL do site atual está hospedada em um localhost ou dentro do github, 
+        //  assim alterando para seu devido diretório
         if (!/127.0.0.1/.test(document.URL)) {
             linkElement.href = `/DWHub/${commonStyleFolder}${file}`;
         } else {
--- js/utils/list_loader.js
+++ js/utils/list_loader.js
@@ -1,7 +1,8 @@
 function fetchDatabase(filePath) {
-    return fetch(filePath)
+    //carrega um arquivo que está guardado dentro do diretório do site, retornando um resultado assíncrono
+    return fetch(filePath) 
     
-    .then(
+    .then( //Esta função está sendo chamada em conjunto com o fetch(filePath), está separado desse jeito por questões de legibilidade
         response => {
             return response.json();
         }
